d3356858c42d9a3f571ace2ff4e69e525b57ae9d
he.yu <he.yu@sfit.shfe.com.cn>
2022-12-01 14:39:41 +0800

    feat: 🎸 开启swagger/开启默认管道

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+import { readJSONSync } from 'fs-extra';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 全局使用管道,用于controller层参数校验
+  app.useGlobalPipes(new ValidationPipe());
+  // cookie解析器
+  app.use(cookieParser());
+  // 开启CORS
+  app.enableCors();
+  // swagger配置
+  const packageJson = readJSONSync('package.json', 'utf8');
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(packageJson.name)
+    .setDescription(packageJson.description)
+    .setVersion(packageJson.version)
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(3000);
 }
 bootstrap();
