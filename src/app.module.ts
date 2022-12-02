40bc7dfa439fafbb94e93607775618a104bd5053
he.yu <he.yu@sfit.shfe.com.cn>
2022-12-02 15:17:01 +0800

    feat: 🎸 添加全局controller日志

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
+import { ControllerLogger } from '../middleware/logger.middleware';
 import { DBConfig } from '../DBConfig';
 
 @Module({
@@ -14,4 +15,8 @@ import { DBConfig } from '../DBConfig';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ControllerLogger).forRoutes('api');
+  }
+}
